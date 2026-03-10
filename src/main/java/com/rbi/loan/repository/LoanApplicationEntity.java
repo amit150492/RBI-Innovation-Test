ead791e280a186a4124b8198116b9a6427023793
AMIT JAIN <mca.amit89@gmail.com>
2026-03-10 12:56:18 +0530

    Feature I added application.properties to connect with database and manually added getter and setter method because having an problem in my local lombok issue

--- src/main/java/com/rbi/loan/repository/LoanApplicationEntity.java
+++ src/main/java/com/rbi/loan/repository/LoanApplicationEntity.java
@@ -2,8 +2,7 @@ package com.rbi.loan.repository;
 
 import com.rbi.loan.model.RiskBand;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -11,8 +10,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "loan_applications")
-@Getter
-@Setter
+
 public class LoanApplicationEntity {
     @Id
     private UUID applicationId;
@@ -36,4 +34,108 @@ public class LoanApplicationEntity {
     private String rejectionReasons; // Stored as comma-separated string
 
     private LocalDateTime createdAt;
+
+    public UUID getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(UUID applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public String getApplicantName() {
+        return applicantName;
+    }
+
+    public void setApplicantName(String applicantName) {
+        this.applicantName = applicantName;
+    }
+
+    public int getApplicantAge() {
+        return applicantAge;
+    }
+
+    public void setApplicantAge(int applicantAge) {
+        this.applicantAge = applicantAge;
+    }
+
+    public BigDecimal getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(BigDecimal monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public RiskBand getRiskBand() {
+        return riskBand;
+    }
+
+    public void setRiskBand(RiskBand riskBand) {
+        this.riskBand = riskBand;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public BigDecimal getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(BigDecimal loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public int getTenureMonths() {
+        return tenureMonths;
+    }
+
+    public void setTenureMonths(int tenureMonths) {
+        this.tenureMonths = tenureMonths;
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public BigDecimal getEmi() {
+        return emi;
+    }
+
+    public void setEmi(BigDecimal emi) {
+        this.emi = emi;
+    }
+
+    public String getRejectionReasons() {
+        return rejectionReasons;
+    }
+
+    public void setRejectionReasons(String rejectionReasons) {
+        this.rejectionReasons = rejectionReasons;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
